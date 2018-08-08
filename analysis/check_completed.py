67dc1613571f0be914a3186f7797be08414a8377
Dominic <email@dominicward.co.uk>
2018-08-08 19:10:42 +0100

    Inspect number of completed pages

--- analysis/check_completed.py
+++ analysis/check_completed.py
@@ -6,7 +6,13 @@ frame = pd.read_csv('../results/dataset.csv')
 
 num_pages_completed = frame.groupby('submission_id')['value'].sum()
 
-print(sum(num_pages_completed == 13))
+print('Number of submissions: ', len(pd.unique(frame.submission_id)))
+
+print('Number of submissions with all completed 13 pages: ',
+      sum(num_pages_completed == 13))
+
+print('Number of submissions with 0 completed pages :',
+      sum(num_pages_completed == 0))
 
 num_pages_completed.hist()
 plt.show()
